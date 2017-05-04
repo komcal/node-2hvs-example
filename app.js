d50b744e5c01a6d14730367bd4ff78a01e0f8c62
komcal <kom.sujautra@gmail.com>
2017-05-04 23:06:13 +0700

    add app

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
